53369d04228ce274037b92b82d209fcb5872148a
mabassi3 <marwa.abassi@ucdenver.edu>
2026-02-03 22:00:00 -0700

    Added server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse text/plain bodies
+app.use(express.text());
+
+app.post('/', (req, res) => {
+    res.send(req.body);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Echo server running on port ${port}`);
+});
+
+module.exports = app;
